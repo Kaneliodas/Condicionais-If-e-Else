1b70ecb00429eb0a8cfa20835fca45fd37bb2c4a
João Vitor Sousa Ramos <79048652+Kaneliodas@users.noreply.github.com>
2024-10-09 07:33:40 -0300

    Create CondicionalIfelse.java

---
+++ CondicionalIfelse.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class CondicionalIfelse {
+    public static void main(String[] args) {
+      int nota = 62;
+      if (nota >= 90) {
+      System.out.println("Conceito A");
+      }
+      else if (nota >= 80) {
+      System.out.println("Conceito B");
+      }
+      else if (nota >= 70) {
+      System.out.println("Conceito C");
+      }
+      else {
+      System.out.println("Conceito D");
+      }
+  }
+}
